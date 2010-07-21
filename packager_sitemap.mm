491fdd78f16e137ebd3105cfc1392c21284bcb99
Sebastiaan Deckers <cbas@pandion.be>
2010-07-21 20:38:48 +0200

    Added more detail in sitemap

--- packager_sitemap.mm
+++ packager_sitemap.mm
@@ -1,49 +1,224 @@
 <map version="0.8.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1279728757669" ID="Freemind_Link_216645198" MODIFIED="1279729061215" STYLE="bubble" TEXT="Packager">
-<node CREATED="1279729064123" ID="_" MODIFIED="1279729626883" POSITION="right" TEXT="Public Site" VSHIFT="-1">
-<node CREATED="1279729131970" ID="Freemind_Link_1768767349" MODIFIED="1279729958227" TEXT="Home">
+<node CREATED="1279728757669" ID="Freemind_Link_216645198" MODIFIED="1279736083662" STYLE="bubble" TEXT="Packager">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1279729064123" ID="_" MODIFIED="1279736083657" POSITION="right" TEXT="Public Site" VSHIFT="-1">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1279729131970" ID="Freemind_Link_1768767349" MODIFIED="1279737476148" TEXT="Home" VSHIFT="-11">
+<font NAME="SansSerif" SIZE="12"/>
 <icon BUILTIN="gohome"/>
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1279735897063" ID="Freemind_Link_1518440115" MODIFIED="1279736513248" TEXT="Title+slogan">
+<font NAME="SansSerif" SIZE="12"/>
 </node>
-<node CREATED="1279729156563" ID="Freemind_Link_1519298957" MODIFIED="1279729161079" TEXT="Features"/>
-<node CREATED="1279729147082" ID="Freemind_Link_783800940" MODIFIED="1279729813293" TEXT="Plans &amp; Pricing"/>
-<node CREATED="1279729184882" ID="Freemind_Link_199352368" MODIFIED="1279729813750" TEXT="Sign Up">
-<node CREATED="1279729424283" ID="Freemind_Link_1200470381" MODIFIED="1279729978291" TEXT="Payment (external)">
+<node CREATED="1279735906967" ID="Freemind_Link_893546790" MODIFIED="1279736513249" TEXT="Service screenshot">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1279735966383" ID="Freemind_Link_1054107220" MODIFIED="1279736513250" TEXT="Top features bullet points">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1279735958407" ID="Freemind_Link_1722374962" MODIFIED="1279736513251" TEXT="Sign Up button">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1279729156563" ID="Freemind_Link_1519298957" MODIFIED="1279737476147" TEXT="Features" VSHIFT="-7">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1279735498631" ID="Freemind_Link_1212176565" MODIFIED="1279736513252" TEXT="Tabbed slides">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1279735525903" ID="Freemind_Link_92748898" MODIFIED="1279736513252" TEXT="Walkthrough video">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1279735545919" ID="Freemind_Link_724678470" MODIFIED="1279736513253" TEXT="Demo account">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1279729147082" ID="Freemind_Link_783800940" MODIFIED="1279737476145" TEXT="Plans &amp; Pricing">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1279735561975" ID="Freemind_Link_1985638196" MODIFIED="1279736513254" TEXT="Comparison table">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1279735626880" ID="Freemind_Link_245108763" MODIFIED="1279736513255" TEXT="Sign up buttons">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1279729184882" ID="Freemind_Link_199352368" MODIFIED="1279737476143" TEXT="Sign Up" VSHIFT="7">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1279729424283" ID="Freemind_Link_1200470381" MODIFIED="1279736083626" TEXT="Payment (external)">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="forward"/>
+</node>
+<node CREATED="1279735646215" ID="Freemind_Link_1251094078" MODIFIED="1279736513257" TEXT="Choose plan">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1279735680663" ID="Freemind_Link_184683458" MODIFIED="1279736513256" TEXT="Enter branded IM client name">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1279735667407" ID="Freemind_Link_1026177031" MODIFIED="1279736513258" TEXT="Enter first/last name">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1279735673032" ID="Freemind_Link_809313994" MODIFIED="1279736513259" TEXT="Enter email address">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1279737391718" ID="Freemind_Link_1429942508" MODIFIED="1279737395938" TEXT="Enter password"/>
+</node>
+</node>
+<node CREATED="1279729077810" ID="Freemind_Link_69777233" MODIFIED="1279737350794" POSITION="right" TEXT="User Area" VSHIFT="25">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1279729190107" ID="Freemind_Link_1258885130" MODIFIED="1279736083614" TEXT="Dashboard">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="gohome"/>
+<icon BUILTIN="pencil"/>
+<node CREATED="1279735709832" ID="Freemind_Link_671545445" MODIFIED="1279736513262" TEXT="Mini stats">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1279735757231" ID="Freemind_Link_31492583" MODIFIED="1279736513263" TEXT="Latest versions snippet">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1279735781048" ID="Freemind_Link_1793551184" MODIFIED="1279736513264" TEXT="Latest downloads">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1279729195515" ID="Freemind_Link_564583632" MODIFIED="1279736083607" TEXT="Appcasting">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1279729460563" ID="Freemind_Link_1540067984" MODIFIED="1279736083606" TEXT="Publishing Updates">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="pencil"/>
+<node CREATED="1279736110327" ID="Freemind_Link_1720168130" MODIFIED="1279736513261" TEXT="(Un)publish tracks to the feed"/>
+<node CREATED="1279736152807" ID="Freemind_Link_382699057" MODIFIED="1279736513260" TEXT="Assign versions to tracks"/>
+<node CREATED="1279736161231" ID="Freemind_Link_1247014890" MODIFIED="1279736513260" TEXT="Synch versions to Pandion IM tracks"/>
+</node>
+<node CREATED="1279729477794" ID="Freemind_Link_283197121" MODIFIED="1279736083604" TEXT="Version Archive">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="pencil"/>
+<node CREATED="1279736220839" ID="Freemind_Link_1919799604" MODIFIED="1279736518815" TEXT="Browse list of versions">
+<node CREATED="1279736265239" ID="Freemind_Link_580202066" MODIFIED="1279736267947" TEXT="Version number"/>
+<node CREATED="1279736269223" ID="Freemind_Link_984718486" MODIFIED="1279736271731" TEXT="Creation date"/>
+<node CREATED="1279736272271" ID="Freemind_Link_1716447260" MODIFIED="1279736344611" TEXT="Installer download link"/>
+<node CREATED="1279736277367" ID="Freemind_Link_1437135858" MODIFIED="1279736295244" TEXT="Build trigger (manual, synch to which track)"/>
+<node CREATED="1279736296575" ID="Freemind_Link_1684212804" MODIFIED="1279736336813" TEXT="Source code download link (custom) or GitHub URL (official)"/>
+<node CREATED="1279736345712" ID="Freemind_Link_392815095" MODIFIED="1279736352283" TEXT="Delete version"/>
+</node>
+</node>
+<node CREATED="1279729482835" ID="Freemind_Link_1279768090" MODIFIED="1279736083602" TEXT="Add Version">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="pencil"/>
+<node CREATED="1279736404751" ID="Freemind_Link_566734047" MODIFIED="1279736409491" TEXT="Choose source code">
+<node CREATED="1279736410431" ID="Freemind_Link_1285536362" MODIFIED="1279736435627" TEXT="Official Pandion IM version (git tag?)"/>
+<node CREATED="1279736437247" ID="Freemind_Link_33000405" MODIFIED="1279736447483" TEXT="Custom code in ZIP archive">
+<node CREATED="1279736448480" ID="Freemind_Link_30247121" MODIFIED="1279736473843" TEXT="Upload custom code archive"/>
+<node CREATED="1279736460320" ID="Freemind_Link_1017896005" MODIFIED="1279736479195" TEXT="Delete previous code archive"/>
+<node CREATED="1279736480487" ID="Freemind_Link_405050084" MODIFIED="1279736488412" TEXT="Download previous code archive"/>
+</node>
+<node CREATED="1279736526375" ID="Freemind_Link_1149052369" MODIFIED="1279736549612" TEXT="Custom git repository">
+<node CREATED="1279736553143" ID="Freemind_Link_1752676034" MODIFIED="1279736555435" TEXT="URL"/>
+<node CREATED="1279736555951" ID="Freemind_Link_981198282" MODIFIED="1279736558371" TEXT="Branch"/>
+</node>
+</node>
+<node CREATED="1279736617424" ID="Freemind_Link_949711478" MODIFIED="1279736624571" TEXT="Branding">
+<node CREATED="1279736626223" ID="Freemind_Link_1602740519" MODIFIED="1279736639739" TEXT="Customize all settings">
+<node CREATED="1279736643487" ID="Freemind_Link_1970748917" MODIFIED="1279736664355" TEXT="&quot;about:settings&quot; UI"/>
+<node CREATED="1279736665855" ID="Freemind_Link_1995008102" MODIFIED="1279736669996" TEXT="Add setting"/>
+<node CREATED="1279736671415" ID="Freemind_Link_520020280" MODIFIED="1279736675155" TEXT="Export settings file"/>
+<node CREATED="1279736675415" ID="Freemind_Link_448385700" MODIFIED="1279736678531" TEXT="Import settings file"/>
+</node>
+<node CREATED="1279736685976" ID="Freemind_Link_678399030" MODIFIED="1279736694939" TEXT="Bundled extras">
+<node CREATED="1279736696471" ID="Freemind_Link_982878246" MODIFIED="1279736734891" TEXT="View bundled extras"/>
+<node CREATED="1279736707071" ID="Freemind_Link_809533529" MODIFIED="1279736712244" TEXT="Remove bundled extras"/>
+<node CREATED="1279736716351" ID="Freemind_Link_1620417273" MODIFIED="1279736730075" TEXT="Search and add extra from the repository"/>
+</node>
+</node>
+<node CREATED="1279736759551" ID="Freemind_Link_1840081867" MODIFIED="1279736765403" TEXT="Version number">
+<node CREATED="1279736767423" ID="Freemind_Link_1499222005" MODIFIED="1279736794523" TEXT="Number spinner controls to enter 3 digits"/>
+<node CREATED="1279736799999" ID="Freemind_Link_409559037" MODIFIED="1279736822315" TEXT="Warn if lower than currently published version"/>
+</node>
+<node CREATED="1279736837311" ID="Freemind_Link_955916960" MODIFIED="1279736840043" TEXT="Build button">
+<node CREATED="1279736843936" ID="Freemind_Link_474467781" MODIFIED="1279736877147" TEXT="Queue the Hudson job"/>
+</node>
+</node>
+</node>
+<node CREATED="1279729227427" ID="Freemind_Link_1030660136" MODIFIED="1279737448126" TEXT="Extras">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1279736568959" ID="Freemind_Link_762221503" MODIFIED="1279736576443" TEXT="TBD"/>
+</node>
+<node CREATED="1279729229539" ID="Freemind_Link_684428474" MODIFIED="1279736083598" TEXT="Statistics">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="pencil"/>
+<node CREATED="1279736912159" ID="Freemind_Link_1342387250" MODIFIED="1279736917931" TEXT="Select date range"/>
+<node CREATED="1279736918655" ID="Freemind_Link_839531090" MODIFIED="1279736942003" TEXT="Export data to common file types (Excel, CSV)"/>
+<node CREATED="1279736956967" ID="Freemind_Link_1374975339" MODIFIED="1279737019684" TEXT="Line graph of active users"/>
+<node CREATED="1279736967600" ID="Freemind_Link_1606144006" MODIFIED="1279737011427" TEXT="Pie chart of software versions in use"/>
+<node CREATED="1279736978655" ID="Freemind_Link_1022392794" MODIFIED="1279737015411" TEXT="Pie chart of update track usage distribution"/>
+</node>
+<node CREATED="1279729235179" ID="Freemind_Link_312049927" MODIFIED="1279736083597" TEXT="Subscription">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="pencil"/>
+<node CREATED="1279737165255" ID="Freemind_Link_720306185" MODIFIED="1279737193148" TEXT="Plan comparison table with &quot;Change Plan&quot; buttons">
+<node CREATED="1279729583107" ID="Freemind_Link_874232599" MODIFIED="1279737210171" TEXT="Payment (external)">
+<font NAME="SansSerif" SIZE="12"/>
 <icon BUILTIN="forward"/>
 </node>
 </node>
+<node CREATED="1279737216863" ID="Freemind_Link_367092901" MODIFIED="1279737240730" TEXT="Cancel subscription"/>
 </node>
-<node CREATED="1279729077810" ID="Freemind_Link_69777233" MODIFIED="1279730315310" POSITION="right" TEXT="User Area" VSHIFT="35">
-<node CREATED="1279729190107" ID="Freemind_Link_1258885130" MODIFIED="1279729961787" TEXT="Dashboard">
+<node CREATED="1279737274926" ID="Freemind_Link_1560381540" MODIFIED="1279737437921" TEXT="Rename Brand">
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1279737286846" ID="Freemind_Link_468057538" MODIFIED="1279737315178" TEXT="Accessed from pulldown menu or link next to brand name in header"/>
+<node CREATED="1279737316095" ID="Freemind_Link_962490263" MODIFIED="1279737329130" TEXT="Check if name is already in use"/>
+</node>
+<node CREATED="1279737354974" ID="Freemind_Link_1500883774" MODIFIED="1279737437919" TEXT="Account details">
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1279737365614" ID="Freemind_Link_393188025" MODIFIED="1279737376746" TEXT="Change first/last name"/>
+<node CREATED="1279737370566" ID="Freemind_Link_1268711862" MODIFIED="1279737373746" TEXT="Change email address"/>
+<node CREATED="1279737379382" ID="Freemind_Link_1321119039" MODIFIED="1279737382050" TEXT="Change password"/>
+</node>
+</node>
+<node CREATED="1279730256643" ID="Freemind_Link_116224965" MODIFIED="1279736083593" POSITION="right" TEXT="Admin" VSHIFT="54">
+<font NAME="SansSerif" SIZE="12"/>
+<node CREATED="1279730288219" ID="Freemind_Link_1486206345" MODIFIED="1279737462509" TEXT="Dashboard">
+<font NAME="SansSerif" SIZE="12"/>
 <icon BUILTIN="gohome"/>
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="button_cancel"/>
 </node>
-<node CREATED="1279729195515" ID="Freemind_Link_564583632" MODIFIED="1279729226583" TEXT="Appcasting">
-<node CREATED="1279729460563" ID="Freemind_Link_1540067984" MODIFIED="1279729476743" TEXT="Publishing Updates"/>
-<node CREATED="1279729477794" ID="Freemind_Link_283197121" MODIFIED="1279729482079" TEXT="Version Archive"/>
-<node CREATED="1279729482835" ID="Freemind_Link_1279768090" MODIFIED="1279729489647" TEXT="Add Version"/>
+<node CREATED="1279730327075" ID="Freemind_Link_1506558332" MODIFIED="1279737462505" TEXT="Brands">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1279730347587" ID="Freemind_Link_162615246" MODIFIED="1279736083588" TEXT="Search">
+<font NAME="SansSerif" SIZE="12"/>
 </node>
-<node CREATED="1279729227427" ID="Freemind_Link_1030660136" MODIFIED="1279729397851" TEXT="Extras"/>
-<node CREATED="1279729229539" ID="Freemind_Link_684428474" MODIFIED="1279729234726" TEXT="Statistics"/>
-<node CREATED="1279729235179" ID="Freemind_Link_312049927" MODIFIED="1279729250318" TEXT="Subscription">
-<node CREATED="1279729583107" ID="Freemind_Link_874232599" MODIFIED="1279729980379" TEXT="Change Plan (external)">
-<icon BUILTIN="forward"/>
+<node CREATED="1279730611787" ID="Freemind_Link_534657978" MODIFIED="1279737462504" TEXT="Versions">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1279730616106" ID="Freemind_Link_1700036161" MODIFIED="1279736083585" TEXT="CRUD"/>
+</node>
+<node CREATED="1279730583138" ID="Freemind_Link_1423116035" MODIFIED="1279736083584" TEXT="CRUD">
+<font NAME="SansSerif" SIZE="12"/>
 </node>
 </node>
+<node CREATED="1279730331659" ID="Freemind_Link_1129337094" MODIFIED="1279737462502" TEXT="Users">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="pencil"/>
+<icon BUILTIN="button_cancel"/>
+<node CREATED="1279730573651" ID="Freemind_Link_247408955" MODIFIED="1279736083581" TEXT="Search">
+<font NAME="SansSerif" SIZE="12"/>
 </node>
-<node CREATED="1279730256643" ID="Freemind_Link_116224965" MODIFIED="1279730324883" POSITION="right" TEXT="Admin" VSHIFT="30">
-<node CREATED="1279730288219" ID="Freemind_Link_1486206345" MODIFIED="1279730318403" TEXT="Dashboard">
-<icon BUILTIN="gohome"/>
+<node CREATED="1279730588020" ID="Freemind_Link_522784630" MODIFIED="1279736083579" TEXT="CRUD">
+<font NAME="SansSerif" SIZE="12"/>
 </node>
-<node CREATED="1279730327075" ID="Freemind_Link_1506558332" MODIFIED="1279730330895" TEXT="Brands">
-<node CREATED="1279730347587" ID="Freemind_Link_162615246" MODIFIED="1279730582574" TEXT="Search"/>
-<node CREATED="1279730611787" ID="Freemind_Link_534657978" MODIFIED="1279730672991" TEXT="Versions">
-<node CREATED="1279730616106" ID="Freemind_Link_1700036161" MODIFIED="1279730617511" TEXT="CRUD"/>
-</node>
-<node CREATED="1279730583138" ID="Freemind_Link_1423116035" MODIFIED="1279730584719" TEXT="CRUD"/>
-</node>
-<node CREATED="1279730331659" ID="Freemind_Link_1129337094" MODIFIED="1279730333391" TEXT="Users">
-<node CREATED="1279730573651" ID="Freemind_Link_247408955" MODIFIED="1279730587167" TEXT="Search"/>
-<node CREATED="1279730588020" ID="Freemind_Link_522784630" MODIFIED="1279730589287" TEXT="CRUD"/>
 </node>
 </node>
 </node>
